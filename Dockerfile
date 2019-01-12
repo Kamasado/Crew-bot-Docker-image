b37ebb0195f08e6e8357c96daa8e81715cb19d9a
Gabriel Lobo <gabrieleduardolobog@gmail.com>
2019-01-12 07:56:46 -0400

    fix

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,7 @@ FROM kamasado/ubuntu_base
 
 USER 0
 
-RUN rm -rf /app
-RUN git clone https://github.com/Kamasado/Crew-bot.git /app
-RUN chown kamasado /app && yarn
+RUN chown kamasado /app
 
 USER kamasado
 
--- image/startpoint.sh
+++ image/startpoint.sh
@@ -4,4 +4,7 @@ echo dk12 | sudo -S echo -- Authorized sudo --
 echo "kamasado:$SSHPASS" | sudo chpasswd
 sudo /etc/init.d/ssh start
 
+git clone https://github.com/Kamasado/Crew-bot.git ./bot
+cd ./bot
+yarn
 yarn start
